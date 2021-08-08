93c3ada435ebf7bd02ca0f27a16857f158064aac
surface_pc <labpc2019@gmail.com>
2021-08-09 00:25:22 +0900

    add TPU Trainer

--- trainer.py
+++ trainer.py
@@ -9,6 +9,10 @@ from tqdm import tqdm_notebook
 from datetime import datetime
 from collections import OrderedDict
 import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.distributed.xla_multiprocessing as xmp
 from evaluate import PSNRMetrics, SAMMetrics
 from pytorch_ssim import SSIM
 from utils import normalize
@@ -22,8 +26,8 @@ torch.set_printoptions(precision=8)
 
 class Trainer(object):
 
-    def __init__(self, model: torch.nn.Module, criterion: torch.nn.Module, 
-                 optimizer: torch.optim.Optimizer, *args, scheduler: =None, 
+    def __init__(self, model: torch.nn.Module, criterion: torch.nn.Module,
+                 optimizer: torch.optim.Optimizer, *args, scheduler: =None,
                  callbacks: list[object]=None, device: str='cpu', **kwargs):
 
         self.model = model
@@ -39,7 +43,7 @@ class Trainer(object):
         self.colab_mode = kwargs.get('colab_mode', False)
         self.scaler = torch.cuda.amp.GradScaler()
 
-    def train(self, epochs: int, train_dataloader: torch.utils.data.DataLoader, 
+    def train(self, epochs: int, train_dataloader: torch.utils.data.DataLoader,
               val_dataloader: torch.utils.data.DataLoader, init_epoch: int=0) -> (np.ndarray, np.ndarray):
 
         if self.colab_mode is False:
@@ -98,7 +102,7 @@ class Trainer(object):
             if self.callbacks:
                 for callback in self.callbacks:
                     callback.callback(self.model, epoch, loss=train_loss,
-                                      val_loss=val_loss, save=True, 
+                                      val_loss=val_loss, save=True,
                                       device=self.device, optim=self.optimizer)
             if self.scheduler is not None:
                 self.scheduler.step()
@@ -111,7 +115,7 @@ class Trainer(object):
     def _trans_data(self, data: torch.Tensor) -> torch.Tensor:
         return data.to(self.device)
 
-    def _step(self, inputs: torch.Tensor, labels: torch.Tensor, 
+    def _step(self, inputs: torch.Tensor, labels: torch.Tensor,
               train: bool=True) -> (torch.Tensor, torch.Tensor):
         with torch.cuda.amp.autocast(self.use_amp):
             output = self.model(inputs)
@@ -134,6 +138,19 @@ class Trainer(object):
         output = output.float().to(self.device)
         output = torch.clamp(output, 0., 1.)
         labels = torch.clamp(label, 0., 1.)
-        return [self.psnr(labels, output).item(), 
-                self.ssim(labels, output).item(), 
+        return [self.psnr(labels, output).item(),
+                self.ssim(labels, output).item(),
                 self.sam(labels, output).item()]
+
+
+class TPUTrainer(Trainer):
+
+    def _step(self, inputs: torch.Tensor, labels: torch.Tensor,
+              train: bool=True) -> (torch.Tensor, torch.Tensor):
+        output = self.model(inputs)
+        loss = self.criterion(output, labels)
+        if train is True:
+            loss.backward()
+            xm.optimizer_step(optimizer, barrier=True)
+            self.optimizer.zero_grad()
+        return loss, output
