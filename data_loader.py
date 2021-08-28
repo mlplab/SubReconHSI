69cae72badb681c661543af3b6e248a2d7dfbbec
surface_pc <labpc2019@gmail.com>
2021-08-29 04:56:40 +0900

    create fusion dataset

--- data_loader.py
+++ data_loader.py
@@ -68,3 +68,48 @@ class PatchEvalDataset(PatchMaskDataset):
         else:
             input_data = measurement_data
         return self.data[idx], input_data, label_data
+
+
+class SpectralFusionDatqaset(torch.utils.data.Dataset):
+
+    def __init__(self, img_path: str, mask_path: str, *args, data_name='CAVE',
+                 concat: bool=False, tanh: bool=False, data_key: str='data',
+                 transform=None, **kwargs) -> None:
+
+        self.img_path = img_path
+        self.data = os.listdir(img_path)
+        self.mask_path = mask_path
+        self.data_len = len(self.data)
+        self.tanh = tanh
+        self.concat = concat
+        self.data_key = data_key
+        self.transforms = transform
+        self.mask_transforms = torchvision.transforms.ToTensor()
+        self.data_name = data_name
+        self.rgb_ch = {'CAVE': (26, 16, 9),
+                       'Harvard': (26, 16, 9),
+                       'ICVL': (26, 16, 9)}
+
+    def __getitem__(self, idx: int) -> (torch.Tensor, torch.Tensor):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        rgb_input = nd_data[self.rgb_ch[self.data_name]]
+        trans_data = nd_data
+        label_data = trans_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+        if self.concat is True:
+            input_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            input_data = measurement_data
+        return (rgb_input, input_data), (rgb_input, label_data)
+
+    def __len__(self):
+        return self.data_len
